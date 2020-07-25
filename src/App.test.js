a2dbcb9cbe32bdbdbf02a6a6516a3a5394dff1a4
Kammanz <kammanz@Kammanzs-MacBook-Pro.local>
2020-07-25 18:37:01 -0400

    [#33] Setup and enzyme and ran a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers/combinedReducers.js";
+import thunk from "redux-thunk";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const store = createStore(reducers, applyMiddleware(thunk));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
+
+  expect(div.innerHTML.length).toEqual(10);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/components/players/list/__tests__/card.test.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import { shallow } from "enzyme";
+// import Card from "components/players/list/card";
+// import Root from "root";
+
+// const wrapped = shallow(
+//   <Root>
+//     <Card />
+//   </Root>
+// );
+
+// it("has an image element within it", () => {
+//   expect(wrapped.find("div").length).toEqual(1);
+// });
