0778da79e0671e507edb6f4f308b746bf4c81f5d
Tumne <freshlygrazed@gmail.com>
2020-08-03 17:06:12 -0400

    [BP-39] Added airbnb eslint styling and readme

--- src/root.js
+++ src/root.js
@@ -1,16 +1,11 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
-import thunk from "redux-thunk";
-import reducers from "reducers/combinedReducers";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import reducers from 'reducers/combinedReducers';
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(
-    reducers,
-    initialState,
-    applyMiddleware(reduxPromise)
-  );
+  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
 
   return <Provider store={store}>{children}</Provider>;
 };
