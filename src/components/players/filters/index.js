3a9948495843a52d7f9f96a883aa79ae91b74ec9
Tumne <freshlygrazed@gmail.com>
2021-04-29 17:31:19 -0400

    controlled react-select

--- src/components/players/filters/index.js
+++ src/components/players/filters/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
+import Select from 'react-select';
 
 import Filter from 'components/_shared/filter';
 import { YEARS, POSITIONS } from 'enums';
@@ -8,6 +9,7 @@ import styles from './index.module.scss';
 
 const Filters = () => {
   const [isUpdated, setUpdate] = useState(false);
+  const [valueB, setValueB] = useState(null);
 
   const beenUpdated = () => {
     setUpdate(true);
@@ -31,6 +33,23 @@ const Filters = () => {
         menuHasChanged={isUpdated}
         update={() => beenUpdated()}
         category={POSITIONS}
+      />
+      <Select
+        onChange={() => {
+          setValueB(null);
+        }}
+        options={[
+          { value: 1, label: 'one' },
+          { value: 2, label: 'two' },
+        ]}
+      />
+      <Select
+        value={valueB}
+        onChange={(option) => setValueB(option)}
+        options={[
+          { value: 3, label: 'three' },
+          { value: 4, label: 'four' },
+        ]}
       />
     </div>
   );
